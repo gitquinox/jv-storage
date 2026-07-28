ffbe95359c19b8e5aa23aebcd05efae63c1bad03
gitquinox <maksumen18@gmail.com>
2026-07-28 17:54:14 +0200

    Completed task storage using generics types

--- checklist.md
+++ checklist.md
@@ -1,4 +1,4 @@
-## Common mistakes (jv-storage)
+    ## Common mistakes (jv-storage)
 
 #### Don't begin class or method implementation with empty line.  
 Remove all redundant empty lines, be careful :)
--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,45 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private final K[] keys;
+    private final V[] values;
+    private int size;
+
+    public StorageImpl() {
+        this.keys = (K[]) new Object[10];
+        this.values = (V[]) new Object[10];
+        this.size = 0;
+    }
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if ((Objects.equals(key, keys[i]))) {
+                values[i] = value;
+                return;
+            }
+        }
+
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if ((Objects.equals(key, keys[i]))) {
+                return values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return this.size;
     }
 }
